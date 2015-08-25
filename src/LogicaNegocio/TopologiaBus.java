be8e5a3046d31a2b1e254d248d0962781c3bdfd4
Crisfalt <Crisfalt@Crisfalt-PC>
2015-08-24 23:01:53 -0500

    dandole

---
+++ src/LogicaNegocio/TopologiaBus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LogicaNegocio;
+
+/**
+ *
+ * @author Crisfalt
+ */
+public class TopologiaBus {
+    
+}
